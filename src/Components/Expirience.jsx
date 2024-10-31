33bc300957c2806b5ef45976254aaba0f657c42f
Rimskiy13 <romanch422@gmail.com>
2024-10-31 07:35:53 -0600

    responsividad arreglada en conocimientos

--- src/Components/Expirience.jsx
+++ src/Components/Expirience.jsx
@@ -1,186 +1,104 @@
-import csharp from './../Images/c.png'
-import cpp from './../Images/cpp.png'
-import java from './../Images/java.png'
-import javascript from './../Images/javascript.png'
-import python from './../Images/python.png'
-import php from './../Images/php.png'
-import react from './../Images/react.png'
-import tailwind from './../Images/tailwind.png'
-import mui from './../Images/mui.png'
-import ant from './../Images/ant.svg'
-import html from './../Images/html.png'
-import css from './../Images/css3.png'
-import mysql from './../Images/mysql.png'
-import postgre from './../Images/postgrees.png'
-import mongo from './../Images/mongo.png'
-import canva from './../Images/canva.png'
-import figma from './../Images/figma.png'
-import trello from './../Images/trello.png'
-import jira from './../Images/jira.png'
-import git from './../Images/github.png'
-
+import csharp from './../Images/c.png';
+import cpp from './../Images/cpp.png';
+import java from './../Images/java.png';
+import javascript from './../Images/javascript.png';
+import python from './../Images/python.png';
+import php from './../Images/php.png';
+import react from './../Images/react.png';
+import tailwind from './../Images/tailwind.png';
+import mui from './../Images/mui.png';
+import ant from './../Images/ant.svg';
+import html from './../Images/html.png';
+import css from './../Images/css3.png';
+import mysql from './../Images/mysql.png';
+import postgre from './../Images/postgrees.png';
+import mongo from './../Images/mongo.png';
+import canva from './../Images/canva.png';
+import figma from './../Images/figma.png';
+import trello from './../Images/trello.png';
+import jira from './../Images/jira.png';
+import git from './../Images/github.png';
 
 function Experience() {
   return (
-    <div className="experience text-center px-5 lg:px-30 gap-5 lg:gap-10 " id="experience">
-      <div className="text-gray-900 text-5xl font-bold">Conocimientos</div>
-      <div className="grid grid-cols-1 gap-8 lg:grid-cols-2  py-10">
-        <div className="ring-1 ring-stone-500 p-10 rounded-3xl py-20">
-          <div className="text-gray-900 p text-3xl font-bold">Lenguajes de Programación</div>
-
-          <div className="grid grid-cols-2 lg:grid-cols-3">
-            <div className="flex py-5">
-              <img src={csharp} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">C#</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={cpp} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">C++</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={java} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">Java</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={javascript} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">JavaScript</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={python} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">Python</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={php} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">PHP</div>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-        <div className="ring-1 ring-stone-500 p-10 rounded-3xl py-20">
-          <div className="text-gray-900 text-3xl font-bold">Frontend</div>
-          <div className="grid grid-cols-2 lg:grid-cols-3">
-            <div className="flex py-5">
-              <img src={react} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">React</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={tailwind} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">Tailwind</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={mui} className="h-10" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">Material UI</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={ant} className="h-10" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">Ant Design</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={html} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">HTML</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={css} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">CSS</div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-        <div className="ring-1 ring-stone-500 p-10 rounded-3xl py-20">
-          <div className="text-gray-900 p text-3xl font-bold">Base de Datos</div>
-          <div className="grid grid-cols-2 lg:grid-cols-3">
-            <div className="flex py-5">
-              <img src={mysql} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">MySQL</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={postgre} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">PostgreSQL</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={mongo} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">MongoDB</div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="ring-1 ring-stone-500 p-10 rounded-3xl py-20">
-          <div className="text-gray-900 p text-3xl font-bold">Herramientas de Diseño</div>
-          <div className="grid grid-cols-2 lg:grid-cols-2 px-20">
-            <div className="flex py-5">
-              <img src={canva} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">Canva</div>
-              </div>
-            </div>
-            <div className="flex py-5">
-              <img src={figma} className="h-15" />
-              <div className="flex flex-col justify-center items-start">
-                <div className="text-gray-900 text-md font-bold">Figma</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="px-20 col-span-2">
-          <div className="ring-1 ring-stone-500  py-20 rounded-3xl ">
-            <div className="text-gray-900 p text-3xl font-bold">Administración de Proyectos</div>
-            <div className="grid grid-cols-2 lg:grid-cols-3 px-10">
-              <div className="flex py-5">
-                <img src={trello} className="h-15" />
-                <div className="flex flex-col justify-center items-start">
-                  <div className="text-gray-900 text-md font-bold">Trello</div>
+    <div className="experience text-center px-5 sm:px-8 lg:px-20 xl:px-40 gap-5 lg:gap-10" id="experience">
+      <div className="text-gray-900 text-3xl sm:text-4xl md:text-5xl font-bold">Conocimientos</div>
+      
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 py-10">
+        {/* Lenguajes de Programación */}
+        <div className="ring-1 ring-stone-500 p-5 sm:p-8 rounded-3xl">
+          <div className="text-gray-900 text-xl sm:text-2xl md:text-3xl font-bold">Lenguajes de Programación</div>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 py-5">
+            {[{ img: csharp, label: 'C#' }, { img: cpp, label: 'C++' }, { img: java, label: 'Java' }, 
+              { img: javascript, label: 'JavaScript' }, { img: python, label: 'Python' }, { img: php, label: 'PHP' }]
+              .map((lang, index) => (
+                <div key={index} className="flex items-center space-x-3 py-3">
+                  <img src={lang.img} className="h-10 md:h-15" alt={lang.label} />
+                  <div className="text-gray-900 text-md font-bold">{lang.label}</div>
                 </div>
-              </div>
-              <div className="flex py-5">
-                <img src={jira} className="h-15" />
-                <div className="flex flex-col justify-center items-start">
-                  <div className="text-gray-900 text-md font-bold">Jira</div>
+              ))}
+          </div>
+        </div>
+        
+        {/* Frontend */}
+        <div className="ring-1 ring-stone-500 p-5 sm:p-8 rounded-3xl">
+          <div className="text-gray-900 text-xl sm:text-2xl md:text-3xl font-bold">Frontend</div>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 py-5">
+            {[{ img: react, label: 'React' }, { img: tailwind, label: 'Tailwind' }, { img: mui, label: 'Material UI' },
+              { img: ant, label: 'Ant Design' }, { img: html, label: 'HTML' }, { img: css, label: 'CSS' }]
+              .map((tech, index) => (
+                <div key={index} className="flex items-center space-x-3 py-3">
+                  <img src={tech.img} className="h-10 md:h-15" alt={tech.label} />
+                  <div className="text-gray-900 text-md font-bold">{tech.label}</div>
                 </div>
-              </div>
-              <div className="flex py-5">
-                <img src={git} className="h-15" />
-                <div className="flex flex-col justify-center items-start">
-                  <div className="text-gray-900 text-md font-bold">GitHub</div>
+              ))}
+          </div>
+        </div>
+        
+        {/* Base de Datos */}
+        <div className="ring-1 ring-stone-500 p-5 sm:p-8 rounded-3xl">
+          <div className="text-gray-900 text-xl sm:text-2xl md:text-3xl font-bold">Base de Datos</div>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 py-5">
+            {[{ img: mysql, label: 'MySQL' }, { img: postgre, label: 'PostgreSQL' }, { img: mongo, label: 'MongoDB' }]
+              .map((db, index) => (
+                <div key={index} className="flex items-center space-x-3 py-3">
+                  <img src={db.img} className="h-10 md:h-15" alt={db.label} />
+                  <div className="text-gray-900 text-md font-bold">{db.label}</div>
                 </div>
-              </div>
-            </div>
+              ))}
           </div>
         </div>
 
+        {/* Herramientas de Diseño */}
+        <div className="ring-1 ring-stone-500 p-5 sm:p-8 rounded-3xl">
+          <div className="text-gray-900 text-xl sm:text-2xl md:text-3xl font-bold">Herramientas de Diseño</div>
+          <div className="grid grid-cols-2 gap-4 py-5">
+            {[{ img: canva, label: 'Canva' }, { img: figma, label: 'Figma' }]
+              .map((designTool, index) => (
+                <div key={index} className="flex items-center space-x-3 py-3">
+                  <img src={designTool.img} className="h-10 md:h-15" alt={designTool.label} />
+                  <div className="text-gray-900 text-md font-bold">{designTool.label}</div>
+                </div>
+              ))}
+          </div>
+        </div>
+
+        {/* Administración de Proyectos */}
+        <div className="ring-1 ring-stone-500 p-5 sm:p-8 rounded-3xl col-span-1 md:col-span-2">
+          <div className="text-gray-900 text-xl sm:text-2xl md:text-3xl font-bold">Administración de Proyectos</div>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 py-5">
+            {[{ img: trello, label: 'Trello' }, { img: jira, label: 'Jira' }, { img: git, label: 'GitHub' }]
+              .map((projectTool, index) => (
+                <div key={index} className="flex items-center space-x-3 py-3">
+                  <img src={projectTool.img} className="h-10 md:h-15" alt={projectTool.label} />
+                  <div className="text-gray-900 text-md font-bold">{projectTool.label}</div>
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
+
 export default Experience;
